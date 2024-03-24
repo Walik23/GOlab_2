7e0aa77dfc0bae64d529cb6d15d810f6e36ef20b
Nikita <115003350+kikoser@users.noreply.github.com>
2024-03-24 12:35:58 +0200

    Update implementation.go

--- implementation.go
+++ implementation.go
@@ -1,9 +1,51 @@
-package lab2
+package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+
+
+type DefaultInfixInterpreter struct {
+}
+
+func (dpc *DefaultInfixInterpreter) isOperator(op string) bool {
+	operators := map[string]bool{"+": true, "-": true, "*": true, "/": true, "^": true}
+	return operators[op]
+}
+
+func (dpc *DefaultInfixInterpreter) ToInfix(prefix string) (string, error) {
+	if prefix == "" {
+		return "", errors.New("empty string")
+	}
+
+	tokens := strings.Fields(prefix)
+	var stack []string
+	for i := len(tokens) - 1; i >= 0; i-- {
+		token := tokens[i]
+		if dpc.isOperator(token) {
+			if len(stack) < 2 {
+				return "", errors.New("expression format is incorrect")
+			}
+			operand1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			operand2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			infixExpr := "(" + operand1 + token + operand2 + ")"
+			stack = append(stack, infixExpr)
+		} else {
+			_, err := strconv.Atoi(token)
+			if err != nil {
+				return "", errors.New("invalid character: " + token)
+			}
+			stack = append(stack, token)
+		}
+	}
+	if len(stack) != 1 {
+		return "", errors.New("expression format is incorrect")
+	}
+	return stack[0], nil
 }
