a3278fcc503e482eb9dd0a0ee71396ac747b92ca
Nikita <115003350+kikoser@users.noreply.github.com>
2024-03-24 12:37:29 +0200

    Update implementation_test.go

--- implementation_test.go
+++ implementation_test.go
@@ -1,4 +1,4 @@
-package lab2
+package main
 
 import (
 	"fmt"
@@ -7,17 +7,100 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+func TestToInfix(t *testing.T) {
+	calculator := DefaultInfixInterpreter{}
+
+	tests := []struct {
+		name       string
+		expression string
+		expected   string
+		wantErr    bool
+	}{
+		{"SimpleAddition", "+ 1 4", "(1+4)", false},
+		{"SimplePow", "^ 3 4", "(3^4)", false},
+		{"SimpleExpression", "/ - 3 4 + 2 7", "((3-4)/(2+7))", false},
+		{"EmptyString", " ", "expression format is incorrect", true},
+		{"InvalidSymbol", "$ 3 4", "invalid character: $", true},
+		{"OneOperand", "5", "5", false},
+		{"ComplexExpression", "+ - 1 2 / 3 4 ", "((1-2)+(3/4))", false},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			got, err := calculator.ToInfix(testCase.expression)
+
+			if testCase.wantErr {
+				assert.Error(t, err, "ToInfix() should return an error")
+			} else {
+				assert.NoError(t, err, "ToInfix() should not return an error")
+				assert.Equal(t, testCase.expected, got, "ToInfix() returned incorrect result")
+			}
+		})
 	}
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+func ExampleDefaultInfixInterpreter_ToInfix() {
+	calculator := DefaultInfixInterpreter{}
+
+	// Example 1: SimpleAddition
+	result, err := calculator.ToInfix("+ 1 4")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println(result)
+
+	// Example 2: SimplePow
+	result, err = calculator.ToInfix("+ - 1 2 / 3 4 ")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println(result)
+
+	// Example 3: SimpleExpression
+	result, err = calculator.ToInfix("/ - 3 4 + 2 7 ")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println(result)
+
+	// Example 4: EmptyString
+	result, err = calculator.ToInfix(" ")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	// Example 5: InvalidSymbol
+	result, err = calculator.ToInfix("$ 3 4")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	// Example 6: OneOperand
+	result, err = calculator.ToInfix("5")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	// Example : ComplexExpression
+	result, err = calculator.ToInfix("+ - 1 2 / 3 4")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println(result)
 
 	// Output:
-	// 2 2 +
+	// (1+4)
+	// ((1-2)+(3/4))
+	// ((3-4)/(2+7))
+	// Error: expression format is incorrect
+	// Error: invalid character: $
+	// ((1-2)+(3/4))
+}
+
+func BenchmarkToInfix(b *testing.B) {
+	calculator := DefaultInfixInterpreter{}
+	expression := "+ 1 4" // Задайте тут ваш вираз для перевірки швидкості
+	for i := 0; i < b.N; i++ {
+		_, _ = calculator.ToInfix(expression)
+	}
 }
