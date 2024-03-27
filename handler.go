0fcdf208aa001e5a654539721c346faa8daed5ed
@Oleh_Ottisko <144347689+OlehOttisko@users.noreply.github.com>
2024-03-27 20:54:59 +0400

    Update handler.go

--- handler.go
+++ handler.go
@@ -1,11 +1,17 @@
 package lab2
 
-// ComputeHandler should be constructed with input io.Reader and output io.Writer.
-// Its Compute() method should read the expression from input and write the computed result to the output.
-type ComputeHandler struct {
-	// TODO: Add necessary fields.
-}
+import (
+	"io"
+)
 
+// Its Compute() method should read the expression from input and write the computed result to the output.
+
+// ComputeHandler обробляє обчислення виразу
+type ComputeHandler struct {
+	Input  io.Reader
+	Output io.Writer
+}
+// Compute обчислює вираз та записує результат
 func (ch *ComputeHandler) Compute() error {
 	// TODO: Implement.
 	return nil
