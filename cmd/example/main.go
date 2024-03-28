96714a2bc881ea722c6a76b0794fe22d7f0aa45e
@Oleh_Ottisko <144347689+OlehOttisko@users.noreply.github.com>
2024-03-28 08:46:28 +0400

    Update main.go

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -5,63 +5,57 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"log"
 	"strings"
+	"go_lab2"
 	lab2 "github.com/roman-mazur/architecture-lab-2"
 )
-
 var (
-	// Оголошення змінних для параметрів командного рядка
-	expressionPtr := flag.String("e", "", "Вираз для обчислення")
-	inputPtr := flag.String("f", "", "Файл з виразом")
-	outputPtr := flag.String("o", "", "Файл для результату")
+	inputExpression = flag.String("e", "", "Expression to compute")
+	fileExpression  = flag.String("f", "", "File with expression")
+	outputFile      = flag.String("o", "", "Output file")
 )
 
 func main() {
-	
 	flag.Parse()
 
-	// Створення екземпляра ComputeHandler
-	var handler ComputeHandler
-	if *expressionPtr != "" {
-		handler.Input = strings.NewReader(*expressionPtr)
-	} else if *inputPtr != "" {
-		file, err := os.Open(*inputPtr)
+	var input io.Reader
+	var output io.Writer
+
+	if *inputExpression != "" {
+		input = strings.NewReader(*inputExpression)
+	} else if *fileExpression != "" {
+		file, err := os.Open(*fileExpression)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Помилка відкриття файлу: %v\n", err)
-			os.Exit(1)
+			fmt.Fprintln(os.Stderr, "Error opening input file:", err)
+			return
 		}
 		defer file.Close()
-		handler.Input = file
+		input = file
 	} else {
-		fmt.Fprintln(os.Stderr, "Необхідно вказати вираз або файл з виразом")
-		os.Exit(1)
+		fmt.Fprintln(os.Stderr, "No input provided")
+		return
 	}
 
-	if *outputPtr != "" {
-		file, err := os.Create(*outputPtr)
+	if *outputFile != "" {
+		file, err := os.Create(*outputFile)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Помилка створення файлу: %v\n", err)
-			os.Exit(1)
+			fmt.Fprintln(os.Stderr, "Error creating output file:", err)
+			return
 		}
 		defer file.Close()
-		handler.Output = file
+		output = file
 	} else {
-		handler.Output = os.Stdout
+		output = os.Stdout
 	}
 
-	// Обчислення виразу
-	err := handler.Compute()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Помилка обробки виразу: %v\n", err)
-		os.Exit(1)
+	handler := lab2.ComputeHandler{
+		Input:      input,
+		Output:     output,
+		Calculator: &lab2.DefaultPostfixCalculator{},
 	}
 
-	       handler := &lab2.ComputeHandler{
-	           Input: {construct io.Reader according the command line parameters},
-	           Output: {construct io.Writer according the command line parameters},
-	       }
-	       err := handler.Compute()
-
-	res, _ := lab2.PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+	if err := handler.Compute(); err != nil {
+		log.Fatalln("main: error occurred for Compute(), error: ", err)
+	}
 }
